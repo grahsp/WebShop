d6189b877affe9aa0b1c8f6e0d4bd8174a2050e8
grahsp <pontus.grahsp@gmail.com>
2025-03-18 21:51:33 +0100

    feat(oauth): add dto object to serialize oauth server response into

---
+++ WebShop.Blazor/DTOs/OAuthTokenResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebShop.Blazor.DTOs
+{
+    public class OAuthTokenResponse
+    {
+        [JsonRequired]
+        [JsonPropertyName("access_token")]
+        public required string AccessToken { get; set; }
+
+
+        [JsonPropertyName("refresh_token")]
+        public string? RefreshToken { get; set; }
+
+        [JsonRequired]
+        [JsonPropertyName("token_type")]
+        public required string TokenType { get; set; }
+
+        [JsonRequired]
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
