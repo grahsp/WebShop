f7b57ce1bf897a39246eeb9850e9d652d6d849b7
Pontus Grahs <pontus.grahsp@gmail.com>
2025-03-19 10:25:12 +0100

    feat(api): add authentication for oauth access token

--- WebShop.Api/Program.cs
+++ WebShop.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace WebShop.Api
 {
     public class Program
@@ -7,8 +9,22 @@ namespace WebShop.Api
             // Add services to the container.
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddUserSecrets<Program>();
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = builder.Configuration["OAuth:Domain"];
+                options.Audience = builder.Configuration["OAuth:Audience"];
+            });
+
+            builder.Services.AddAuthorization();
 
             // Configure the HTTP request pipeline.
             var app = builder.Build();
@@ -17,6 +33,11 @@ namespace WebShop.Api
                 app.MapOpenApi();
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapGet("/", () => "OK").RequireAuthorization();
 
             app.Run();
         }
